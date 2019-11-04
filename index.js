ef989fb38d485e629f1e65763b0ae665ab1208f1
andreykokorev <andrey.kokorev.w.dev@gmail.com>
2019-11-05 03:14:57 +0700

    feat: add eslint

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
-
 const keyBoard = {
-  lang: "en",
+  lang: localStorage.getItem('lang') || "en",
   langCode: {
     "ru": 2,
     "en": 0
@@ -11,16 +10,16 @@ const keyBoard = {
   buttonsCodes: [
     192, 49, 50, 51, 52, 53, 54, 55, 56, 57, 48, 189, 187, 8,
     9, 81, 87, 69, 82, 84, 89, 85, 73, 79, 80, 219, 221, 13,
-    20, 65, 83, 68, 70, 71, 72, 74, 75, 76, 186, 222, 220,
-    16, 90, 88, 67, 86, 66, 78, 77, 188, 190, 191, 16, 38,
-    17, 91, 18, 32, 18, 17, 37, 40, 39
+    20, 65, 83, 68, 70, 71, 72, 74, 75, 76, 186, 222, 220, 46,
+    16, 90, 88, 67, 86, 66, 78, 77, 188, 190, 191, 38,  16,
+    17, 91, 18, 32, 18, 37, 40, 39, 17
   ],
   buttonsSigns: [
     ["`", "~", ""], ["1", "!", ""], ["2", "@", ""], ["3", "#", ""], ["4", "$", ""], ["5", "%", ""], ["6", "^", ""], ["7", "&", ""], ["8", "*", ""], ["9", "(", ""], ["0", ")", ""], ["-", "_", ""], ["=", "+", ""], ["Backspace", ""], 
     ["Tab", "", ""], ["q", "", "й"], ["w", "", "ц"], ["e", "", "у"], ["r", "", "к"], ["t", "", "е"], ["y", "", "н"], ["u", "", "г"], ["i", "", "ш"], ["o", "", "щ"], ["p", "", "з"], ["[", "{", "х"], ["]", "}", "ъ"], ["Enter", "", ""], 
-    ["Caps Lock", "", ""], ["a", "", "ф"], ["s", "", "ы"], ["d", "", "в"], ["f", "", "а"], ["g", "", "п"], ["h", "", "р"], ["j", "", "о"], ["k", "", "л"], ["l", "", "д"], [";", ":", "ж"], ["'", "\"", "э"], ["\\", "|", ","], 
-    ["Shift", "", ""], ["z", "", "я"], ["x", "", "ч"], ["c", "", "с"], ["v", "", "м"], ["b", "", "и"], ["n", "", "т"], ["m", "", "ь"], [",", "<", "б"], [".", ">", "ю"], ["/", "?", "."], ["Shift", "", ""], ["Up", "", ""],
-    ["Ctrl", "", ""] , ["Wn", "", ""], ["Alt", "", ""], ["Space", "", ""], ["RAlt", "", ""], ["RCtrl", "", ""], ["Left", "", ""], ["Down", "", ""], ["Right", "", ""]
+    ["Caps Lock", "", ""], ["a", "", "ф"], ["s", "", "ы"], ["d", "", "в"], ["f", "", "а"], ["g", "", "п"], ["h", "", "р"], ["j", "", "о"], ["k", "", "л"], ["l", "", "д"], [";", ":", "ж"], ["'", "\"", "э"], ["\\", "|", ","], ["Del", "", ""],
+    ["Shift", "", ""], ["z", "", "я"], ["x", "", "ч"], ["c", "", "с"], ["v", "", "м"], ["b", "", "и"], ["n", "", "т"], ["m", "", "ь"], [",", "<", "б"], [".", ">", "ю"], ["/", "?", "."],  ["Up", "", ""], ["Shift", "", ""],
+    ["Ctrl", "", ""] , ["Wn", "", ""], ["Alt", "", ""], ["Space", "", ""], ["Alt", "", ""], ["Left", "", ""], ["Down", "", ""], ["Right", "", ""], ["Ctrl", "", ""]
   ],
 
   renderKeyboard: function () {
@@ -29,16 +28,19 @@ const keyBoard = {
     let keyboardArea = document.createElement('div');
     let textArea = document.createElement('input');
     let button = '';
-    let langIndex = keyBoard.langCode[keyBoard.lang];
+    let langIndex = this.langCode[this.lang];
 
     for (let i = 0; i < keyBoard.buttonsCodes.length; i++) { 
-      button = `<div class="button" id="${keyBoard.buttonsCodes[i]}">${langPanel(i, langIndex)}</div>`;
+      button = `<div class="button" id="${this.buttonsCodes[i]}">${langPanel(i, langIndex)}</div>`;
       keyboardArea.innerHTML += button;
     }
 
-    fragment.appendChild(keyboardArea).classList.add('keyboard-area');
+    textArea.setAttribute("autofocus",'true');
+    textArea.setAttribute("cols", 50);
+    textArea.setAttribute("rows", 10);
+
     keyboardArea.appendChild(textArea).classList.add('text-area');
-    textArea.setAttribute("autofocus",'true')
+    fragment.appendChild(keyboardArea).classList.add('keyboard-area');
     body.appendChild(fragment);  
 
     function langPanel(i, langIndex) {
@@ -54,99 +56,118 @@ const keyBoard = {
       }
     }
 
-    keyBoard.input(body,keyboardArea, textArea);
+    this.setListeners(body,keyboardArea, textArea);
+    this.shift.keyboardArea = keyboardArea;
   }, 
-
-  input: function (body, keyboardArea, textArea) {
+  setListeners: function (body, keyboardArea, textArea) {
     let button;
-    body.addEventListener('keydown', function(e) {
+
+    keyboardArea.addEventListener('keydown', (e)=>{
+      e.preventDefault();
       button = keyboardArea.querySelector(`[id ="${e.keyCode}"]`);
-
-      onInput("keyup", button, e, textArea); 
+      this.onPress( button, e, textArea, keyboardArea); 
     })
 
-    keyboardArea.addEventListener('mousedown', function(e) {
-      if (e.target !== body || e.target !== textArea) {
-      button = e.target;
-      }
-      onInput("mouseup", button, e, textArea);
-
-     
+    keyboardArea.addEventListener("keyup", (e) => {
+      button = keyboardArea.querySelector(`[id ="${e.keyCode}"]`); 
+      if (e.keyCode !== 20 ) this.outPress( button, e);
     })
-    function onInput(event, button, e) {    
-      button.classList.add('active');
 
-      textArea.textContent += (button.textContent.length < 2) ? button.textContent : ''; 
+    body.addEventListener('keydown', () => {
+      textArea.focus(); 
+    })
+       
+    keyboardArea.addEventListener('mousedown', (e) => {       
+      button = e.target;      
+      this.onPress(button, e, textArea, keyboardArea);
+    })
 
-      if ( e.keyCode === 16 ) keyBoard.shiftPress = true;
-      if ( e.keyCode === 17 ) keyboardArea.ctrlPress = true;
-      
-      if ( e.keyCode === 20 ) {
-        if (keyBoard.capsLock === false) { 
-          keyBoard.capsLock = true;
-         } else { 
-           keyBoard.capsLock = false;
-           shift(false);
-           button.classList.remove('active');          
-         }
+    keyboardArea.addEventListener('mouseup', (e) => {       
+      button = e.target;      
+      this.outPress(button, e, textArea);
+    })
+  },
+  onPress: function ( button, e, textArea, keyboardArea) {  
+    button.classList.add('active');
+
+    if ( e.shiftKey || button.id === "16") {
+      this.shiftPress = true;
+      this.compare();
+    }   
+    if ( e.keyCode === 17 || button.id === "17" ) this.ctrlPress = true;
+    if ( e.keyCode === 20 || button.id === "20" ) {
+      if ( this.capsLock === false ) { 
+        this.capsLock = true;
+        this.compare()
+       } else { 
+         this.capsLock = false;
+         this.compare();   
+         button.classList.remove('active');      
+       }
+    }
+
+    if ( e.keyCode === 18 && e.ctrlKey ) {
+      console.log(e.ctrlKey)
+      this.lang = ( this.lang === "ru") ?  "en" : "ru";
+      this.changeLang(keyboardArea);
+    }
+  
+    if ( e.keyCode === 8 || button.id === "8" ) textArea.value = textArea.value.slice(0, -1);     
+    if ( e.keyCode === 32 || button.id === "32" ) textArea.value += " ";
+    if ( button.textContent.length < 2 ) textArea.value += button.textContent;     
+  },  
+  outPress: function ( button, e ) {
+    if ( e.shiftKey === false || button.id === "16" ) {
+       this.shiftPress = false;      
+       this.shift(false);
+       this.compare();
+      }                  
+      if ( e.keyCode === 17 || button.id === '17' ) this.ctrlPress = false;    
+      button.classList.remove('active');
+  },
+  compare: function (){
+    if ( this.capsLock === false && this.shiftPress === false )  this.shift(false);
+    if ( this.capsLock === true && this.shiftPress === true )  this.shift(false);
+    if ( this.capsLock === false && this.shiftPress === true )  this.shift(true);
+    if ( this.capsLock === true && this.shiftPress === false )  this.shift(true);
+
+  },
+  changeLang: function ( keyboardArea ) {
+    
+    let text;
+    for (let i = 0; i < this.buttonsSigns.length; i++) {
+      if ( this.lang === "ru" ) {
+        if (this.buttonsSigns[i][2]) {
+          text = this.buttonsSigns[i][2];
+          keyboardArea.querySelector(`[id ="${this.buttonsCodes[i]}"]`).textContent = text;
+        }
       }
-     
-      if ( e.keyCode !== 20 ) {
-        body.addEventListener(event, function() {
-          if (e.keyCode === 16) {
-            keyBoard.shiftPress = false;
-            if (keyBoard.capsLock !==true) {
-              shift(false);
-            }
-          } 
-          if (e.keyCode === 17) keyboardArea.ctrlPress = false;
-          button.classList.remove('active'); 
-        })
-      }   
-      if (e.keyCode === 18 ) {
-        keyBoard.lang = ( keyBoard.lang === "ru") ?  "en" : "ru";
-        changeLang();
-      } 
-      if ( keyBoard.capsLock === true || keyBoard.shiftPress === true ) {
-        shift(true);
+      if ( this.lang === "en" ) {
+        if (this.buttonsSigns[i][2]) {
+          text = this.buttonsSigns[i][0];
+          keyboardArea.querySelector(`[id ="${this.buttonsCodes[i]}"]`).textContent = text;        
+        }
       }
     }
-    function changeLang() {
-      for (let i = 0; i < keyBoard.buttonsSigns.length; i++) {
-        if ( keyBoard.lang === "ru" ) {
-          if (keyBoard.buttonsSigns[i][2]) {
-            text = keyBoard.buttonsSigns[i][2];
-            keyboardArea.querySelector(`[id ="${keyBoard.buttonsCodes[i]}"]`).textContent = text;
-          }
-        }
-        if ( keyBoard.lang === "en" ) {
-          if (keyBoard.buttonsSigns[i][2]) {
-            text = keyBoard.buttonsSigns[i][0];
-            keyboardArea.querySelector(`[id ="${keyBoard.buttonsCodes[i]}"]`).textContent = text;
-            if (keyBoard.buttonsSigns[i][2]) {
-              
-            }
-          }
-        }
-      }
-      
-    }
-    function shift(condition) {
-      let text = '';
+    localStorage.setItem("lang", this.lang);
+  },
+  shift: function (condition) { 
+      let text;
+
       if (keyBoard.lang === "ru") {
         for (let i = 0; i < keyBoard.buttonsSigns.length; i++) {
   
           if ( keyBoard.buttonsSigns[i][2] ) {
             if (condition) {
-              text = keyboardArea.querySelector(`[id ="${keyBoard.buttonsCodes[i]}"]`).textContent.toUpperCase();
-            } else { text = keyboardArea.querySelector(`[id ="${keyBoard.buttonsCodes[i]}"]`).textContent.toLowerCase();
+              text = this.shift.keyboardArea.querySelector(`[id ="${keyBoard.buttonsCodes[i]}"]`).textContent.toUpperCase();
+            } else { text = this.shift.keyboardArea.querySelector(`[id ="${keyBoard.buttonsCodes[i]}"]`).textContent.toLowerCase();
             }        
-            keyboardArea.querySelector(`[id ="${keyBoard.buttonsCodes[i]}"]`).textContent = text;
+            this.shift.keyboardArea.querySelector(`[id ="${keyBoard.buttonsCodes[i]}"]`).textContent = text;
           } else if ( keyBoard.buttonsSigns[i][1] ) {
             if(condition) {
-              keyboardArea.querySelector(`[id ="${keyBoard.buttonsCodes[i]}"]`).textContent = keyBoard.buttonsSigns[i][1];
+              this.shift.keyboardArea.querySelector(`[id ="${keyBoard.buttonsCodes[i]}"]`).textContent = keyBoard.buttonsSigns[i][1];
             } else {
-              keyboardArea.querySelector(`[id ="${keyBoard.buttonsCodes[i]}"]`).textContent = keyBoard.buttonsSigns[i][0];
+              this.shift.keyboardArea.querySelector(`[id ="${keyBoard.buttonsCodes[i]}"]`).textContent = keyBoard.buttonsSigns[i][0];
             }          
           }
         }
@@ -156,22 +177,21 @@ const keyBoard = {
   
           if ( keyBoard.buttonsSigns[i][1] ) {
             if (condition) {
-              keyboardArea.querySelector(`[id ="${keyBoard.buttonsCodes[i]}"]`).textContent = keyBoard.buttonsSigns[i][1]; 
+              this.shift.keyboardArea.querySelector(`[id ="${keyBoard.buttonsCodes[i]}"]`).textContent = keyBoard.buttonsSigns[i][1]; 
             } else {
-              keyboardArea.querySelector(`[id ="${keyBoard.buttonsCodes[i]}"]`).textContent = keyBoard.buttonsSigns[i][0]; 
+              this.shift.keyboardArea.querySelector(`[id ="${keyBoard.buttonsCodes[i]}"]`).textContent = keyBoard.buttonsSigns[i][0]; 
             }
               
           } else if ( keyBoard.buttonsSigns[i][0] >= "a" &&  keyBoard.buttonsSigns[i][0] <= "z") {
             if(condition){ 
-              text = keyboardArea.querySelector(`[id ="${keyBoard.buttonsCodes[i]}"]`).textContent.toUpperCase();
+              text =this.shift.keyboardArea.querySelector(`[id ="${keyBoard.buttonsCodes[i]}"]`).textContent.toUpperCase();
             } else {
-              text = keyboardArea.querySelector(`[id ="${keyBoard.buttonsCodes[i]}"]`).textContent.toLowerCase();
+              text = this.shift.keyboardArea.querySelector(`[id ="${keyBoard.buttonsCodes[i]}"]`).textContent.toLowerCase();
             }          
-              keyboardArea.querySelector(`[id ="${keyBoard.buttonsCodes[i]}"]`).textContent = text;
+            this.shift.keyboardArea.querySelector(`[id ="${keyBoard.buttonsCodes[i]}"]`).textContent = text;
           } 
         }
       }
-    }
   }
 }
 keyBoard.renderKeyboard();
